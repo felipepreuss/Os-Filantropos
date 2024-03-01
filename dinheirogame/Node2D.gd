b607afe12bb7ec5e8cd3231351328e0d6bae90c7
VaporeonLover28 <161027878+VaporeonLover28@users.noreply.github.com>
2024-03-01 08:39:12 -0300

    gravidade, safe zone, teclas funcionando
    arte ainda placeholder
    tem que fazer o sistema de dinheiro

--- dinheirogame/Node2D.gd
+++ dinheirogame/Node2D.gd
@@ -1,4 +1,4 @@
-extends Label
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	text = "gravidade: " + str(get_node("../Zeppellin").newgravity)
+	pass
--- dinheirogame/Zeppellin.gd
+++ dinheirogame/Zeppellin.gd
@@ -2,27 +2,48 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -150.0
+var JUMP_VELOCITY = -75.0
 var newgravity = 290
+var can_press_right = true
+var can_press_a = true
 
 func _ready():
 	$GravChanger.start()
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += newgravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	#if Input.is_action_just_pressed("ui_accept"):
+		#velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_just_pressed("A") && can_press_a == true:
 		velocity.y = JUMP_VELOCITY
+		can_press_a = false
+		can_press_right = true
+
+	if Input.is_action_just_pressed("Direita") && can_press_right == true:
+		velocity.y = JUMP_VELOCITY
+		can_press_a = true
+		can_press_right = false
 
 	move_and_slide()
 
 
 func _on_death_zone_body_entered(body):
-	pass
-
-
+	get_tree().change_scene_to_file("res://gameover.tscn")
+	
 func _on_grav_changer_timeout():
-	newgravity += 5
+	newgravity *= 1.1
+
+func _on_safe_zone_body_entered(body):
+	JUMP_VELOCITY = JUMP_VELOCITY/2
+
+func _on_safe_zone_body_exited(body):
+	JUMP_VELOCITY *= 2
+
+func _on_safe_zone_2_body_entered(body):
+	JUMP_VELOCITY = JUMP_VELOCITY/5
+
+func _on_safe_zone_2_body_exited(body):
+	JUMP_VELOCITY *= 5
